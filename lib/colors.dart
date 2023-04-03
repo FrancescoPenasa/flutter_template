43cb6a94160567fbdabc59292b90194cebc69133
FrancescoPenasa <francesco.penasa.job@gmail.com>
2023-04-03 10:50:51 +0200

    add custom colors

---
+++ lib/colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> colorPrimaryCodes = {
+  50: const Color.fromRGBO(141, 198, 63, .1),
+  100: const Color.fromRGBO(141, 198, 63, .2),
+  200: const Color.fromRGBO(141, 198, 63, .3),
+  300: const Color.fromRGBO(141, 198, 63, .4),
+  400: const Color.fromRGBO(141, 198, 63, .5),
+  500: const Color.fromRGBO(141, 198, 63, .6),
+  600: const Color.fromRGBO(141, 198, 63, .7),
+  700: const Color.fromRGBO(141, 198, 63, .8),
+  800: const Color.fromRGBO(141, 198, 63, .9),
+  900: const Color.fromRGBO(141, 198, 63, 1),
+};
+MaterialColor colorPrimary = MaterialColor(0xff3ca0d8, colorPrimaryCodes);
+
+Map<int, Color> colorBgCodes = {
+  50: const Color.fromRGBO(255, 255, 255, .1),
+  100: const Color.fromRGBO(255, 255, 255, .2),
+  200: const Color.fromRGBO(255, 255, 255, .3),
+  300: const Color.fromRGBO(255, 255, 255, .4),
+  400: const Color.fromRGBO(255, 255, 255, .5),
+  500: const Color.fromRGBO(255, 255, 255, .6),
+  600: const Color.fromRGBO(255, 255, 255, .7),
+  700: const Color.fromRGBO(255, 255, 255, .8),
+  800: const Color.fromRGBO(255, 255, 255, .9),
+  900: const Color.fromRGBO(255, 255, 255, 1.0),
+};
+MaterialColor colorBg = MaterialColor(0xFfE8F4D8, colorBgCodes);
+
+Map<int, Color> colorBlackCodes = {
+  50: const Color.fromRGBO(0, 0, 0, 0.1),
+  100: const Color.fromRGBO(0, 0, 0, .2),
+  200: const Color.fromRGBO(0, 0, 0, .3),
+  300: const Color.fromRGBO(0, 0, 0, .4),
+  400: const Color.fromRGBO(0, 0, 0, .5),
+  500: const Color.fromRGBO(0, 0, 0, .6),
+  600: const Color.fromRGBO(0, 0, 0, .7),
+  700: const Color.fromRGBO(0, 0, 0, .8),
+  800: const Color.fromRGBO(0, 0, 0, .9),
+  900: const Color.fromRGBO(0, 0, 0, 1.0),
+};
+MaterialColor colorBlack = MaterialColor(0xFf000000, colorBlackCodes);
